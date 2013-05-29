0a04859367b19da24f77ea0c86904339b12ff707
unknown <vsialiuk@CD01-FM0MYN1.capiqcorp.com>
2013-05-29 13:11:10 -0400

    spin lock stub added

--- concurrent/spin_lock.h
+++ concurrent/spin_lock.h
@@ -1,1 +1,24 @@
 //empty file
+#include <atomic>
+
+
+namespace concurrent
+{
+
+class spin_lock
+{
+public:
+    spin_lock() : spin_(0) {}
+
+    void lock() {}
+    void unlock() {}
+
+private:
+    bool try_lock() {}
+private:
+    std::atomic<int> spin_;
+};
+	
+}//namespace concurrent
+
+
---
+++ tests/spin_lock_test.cpp
@@ -0,0 +1,6 @@
+#include <concurrent/spin_lock.h>
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
